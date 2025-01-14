f533006abf09a4842192be223a7e42a752766a6f
Tommaso Magarotto <Tommymaga01@gmail.com>
2025-01-14 18:57:01 +0100

    Create PROBABILITY DISTRIBUTION.R

---
+++ codes/PROBABILITY DISTRIBUTION.R
@@ -0,0 +1,13 @@
+#DISTRIBUTIONS IN R
+
+dnorm() # point probability (or density probability) distribution
+pnorm() # cumulative probability
+qnorm() # theoretical quantiles
+rnorm() # random number extraction
+
+#PROBABILITY DISTRIBUTION:
+
+dbinom() 
+dnorm()
+
+dbinom(7,size=10,prob=0.5) 
