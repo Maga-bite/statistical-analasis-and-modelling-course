a870c493829d57f7e6041c12abad32276d6c7ddd
Tommaso Magarotto <Tommymaga01@gmail.com>
2025-01-28 18:14:53 +0100

    Update and rename excercise_4_iris.R to excercise_1iris.R

--- codes/excercise_1iris.R
+++ codes/excercise_1iris.R
@@ -1,4 +1,106 @@
-#Finally, perform a hierarchical clustering of the “iris” dataset applying the
+###ESERCIZIO 1
+# Install and load necessary package
+install.packages("dichromat")
+library(dichromat)
+
+# Define the original color palette
+original_colors <- c("red", "blue", "green")
+
+# Simulate color palettes for different types of colorblindness
+protan <- dichromat(original_colors, type = "protan")
+deutan <- dichromat(original_colors, type = "deutan")
+tritan <- dichromat(original_colors, type = "tritan")
+
+# Loading the iris dataset
+iris_table <- iris
+species_names <- unique(iris_table$"Species")
+
+# Print species info
+print(species_names)
+table(iris_table$"Species")
+
+# Map species to original colors and colorblind-simulated colors
+species_colours_original <- c("setosa" = "red", 
+                              "versicolor" = "blue", 
+                              "virginica" = "green")
+
+species_colours_deutan <- c("setosa" = deutan[1], 
+                            "versicolor" = deutan[2], 
+                            "virginica" = deutan[3])
+
+# Shapes for the species (consistent across plots)
+species_shapes <- c("setosa" = 17, "versicolor" = 15, "virginica" = 19)
+
+
+# Creating scatterplots
+
+# 1
+
+plot(iris$Petal.Length, iris$Petal.Width, 
+     col = species_colours_deutan[iris$Species], 
+     pch = species_shapes[iris$Species], 
+     xlab = "Petal Length(cm)", ylab = "Petal Width(cm)", asp = 2,
+     main = "Petal Length vs Width")
+legend("topleft", legend = names(species_colours_deutan), 
+       col = species_colours_deutan, 
+       pch = species_shapes, cex = 1)
+
+#2
+plot(iris$Sepal.Length, iris$Sepal.Width, 
+     col = species_colours_deutan[iris$Species], 
+     pch = species_shapes[iris$Species], 
+     xlab = "Sepal Length(cm)", ylab = "Sepal Width(cm)", asp = 2, 
+     main = "Sepal Length vs Width")  
+legend("topleft", legend = names(species_colours_deutan), 
+       col = species_colours_deutan, 
+       pch = species_shapes, cex = 1) 
+
+#3
+
+# Setting up the plotting area to produce a multi-panel plot with 1 row and 2 columns.
+par(mfrow = c(1, 2), oma = c(0, 0, 3, 0), mar = c(5, 4, 4, 2) + 0.1) # Adjusting outer and inner margins
+
+
+plot(iris$Petal.Length, iris$Petal.Width, 
+     col = species_colours_deutan[iris$Species], 
+     pch = species_shapes[iris$Species], 
+     xlab = "Petal Length(cm)", ylab = "Petal Width(cm)", asp = 2,
+     main = "Petal Length vs Width")
+legend("topleft", legend = names(species_colours_deutan), 
+       col = species_colours_deutan, 
+       pch = species_shapes, cex = 1)
+
+plot(iris$Sepal.Length, iris$Sepal.Width, 
+     col = species_colours_deutan[iris$Species], 
+     pch = species_shapes[iris$Species], 
+     xlab = "Sepal Length(cm)", ylab = "Sepal Width(cm)", asp = 2, 
+     main = "Sepal Length vs Width")  
+legend("topleft", legend = names(species_colours_deutan), 
+       col = species_colours_deutan, 
+       pch = species_shapes, cex = 1) 
+
+# Adding a common title for both plots.
+mtext("Petal and Sepal Scatterplots", side = 3, outer = TRUE, 
+      line = -1, cex = 1.5) # Adds a title to the entire plotting area.
+
+# Resetting plotting parameters to avoid that future subsequent plots might 
+#nherit the multi-panel layout.
+par(mfrow = c(1, 1))
+
+
+
+
+
+
+
+
+
+
+
+################################################################################################################################################################
+###ESERCIZIO 4
+
+#Finally, perform a hierarchical clustering of the “iris” dataset by applying the
 #“complete” method. Compare the obtained three clusters to the original
 #species classification and provide a graphical result by recoloring one of the
 #scatterplots produced in step 1 with the new classification. Comment on the
@@ -39,6 +141,9 @@ SW <- "Sepal.Width"
 PL <- "Petal.Length" 
 PW <- "Petal.Width"  
 SP <- "Species"
+
+
+--------------------------------------------------------------------
 
 iris_table <- iris
 
@@ -176,3 +281,4 @@ create_colorblind_friendly_dendrogram <- function(data, species_col) {
 
 # Esempio di utilizzo della funzione
 create_colorblind_friendly_dendrogram(iris, "Species")
+
